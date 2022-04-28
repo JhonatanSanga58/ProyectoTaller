10df3f1f58e2d571c84dc4d3d44e97e7630ee189
jhonatansanga58 <sbj2019949@est.univalle.edu>
2022-04-27 23:24:52 -0400

    creating master page view

---
+++ app/Views/master/footer.php
@@ -0,0 +1,26 @@
+	<footer>
+		<div class="about">
+			<img class="logofooter" src="<?php echo base_url('sources/images/logo.png') ?>" alt="AutoTest logo">
+			<nav>
+				<h1>Elaborado por:</h1>
+				<ul>
+					<li>
+						<p>
+							Martinez Mendoza Jhasmani Brayan
+						</p>
+					</li>
+					<li>
+						<p>
+							Sanga Balcazar Jhonatan Levi
+						</p>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="date">
+			<p>Cochabamba - 2022 todos los derechos reservados, te lo prometo</p>
+		</div>
+	</footer>
+</body>
+
+</html>
