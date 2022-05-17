08c6cf84ec9f6875cf504624a6318fed37ff17e5
Jhasmani Martinez <jhas20010520@gmail.com>
2022-05-17 01:25:05 -0400

    view register student

--- app/Controllers/Student.php
+++ app/Controllers/Student.php
@@ -7,11 +7,29 @@ class Student extends BaseController
     public function index()
     {
         echo view('master/header');
-        echo view('student_register');
+        echo view('student/student_register');
         echo view('master/footer');
     }
     public function register()
     {
-    	
+        $session = session();
+
+        if (!$session->has('role')) 
+        {
+        	echo view('master/header');
+            echo view('student/student_register');
+            echo view('master/footer');
+        }
+        else 
+        {
+            $url = base_url('public');
+            return redirect()->to($url);
+        }
+    }
+    public function registerModel()
+    {
+        echo view('master/header');
+        echo view('student/student_register');
+        echo view('master/footer');
     }
 }
